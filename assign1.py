5df1830b8a734ee6c5804bfa58918a587de1519a
Aledrin <160062603+Aledrin@users.noreply.github.com>
2025-05-05 22:02:07 -0500

    Add files via upload

---
+++ assign1.py
@@ -0,0 +1,22 @@
+surname = "Cantuna"
+age = 23
+hobby = "training"
+
+print("Hi there I am Mr",surname,"recently I am",age,"and one of my hobbies is",hobby)
+
+num1 = 5
+num2 = 3
+print("If",num1,"people","brings",num2,"manzanas then we have:",num1 * num2)
+
+num= int(input("Give me a number: "))
+
+if num > 0:
+
+    print("This number is positive. Awesome!")
+
+elif num == 0:
+
+    print("Zero Zero it is. A perfect balance!")
+else:
+
+    print("This number is negative")
