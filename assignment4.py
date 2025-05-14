971050e3386a02348665343ee34556b07b19586f
Aledrin <160062603+Aledrin@users.noreply.github.com>
2025-05-13 19:11:00 -0500

    Add files via upload

---
+++ assignment4.py
@@ -0,0 +1,31 @@
+#Task 1
+fruit=["apple", "orange", "banana", "coco", "cherry"]
+print("elements in the list: ",len(fruit))
+print("Original list: ",fruit)
+fruit.append("strawberry")
+print("After adding a fruit: ",fruit)
+fruit.remove("apple")
+print("After removing a fruit: ",fruit)
+fruit[0] = "pineapple"
+print("After change a fruit: ",fruit)
+print("reversed list: ",fruit[::-1])
+#Task 2
+info = {"name": "David", "age": 23, "city": "Phoenix"}
+print("original:",info)
+info["favorite color"] = "Red"
+info["city"] = "Guadalajara"
+print("modified:",info)
+
+del info["age"]
+for key, value in info.items():
+    print(f"{key}: {value}")
+
+#Task 3
+tup = (1, 2, 3)
+print(tup)
+try:
+    tup[0] = "Interstellar"
+except TypeError as e:
+    print(f"Error: {e}")
+
+print(f"Length of the tuple: {len(tup)}")
